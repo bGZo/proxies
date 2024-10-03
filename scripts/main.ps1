2fd5139c36211d81227b19d4bd62833752834a36
菜就多練練 <im@bgzo.cc>
2024-10-03 15:54:25 +0800

    feat: fetch lastest released

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -12,13 +12,13 @@ $env:cwd_data = $env:cwd_root + "data/"
 
 function Show-Menu {
     Clear-Host
-    echo "Choose operation:"
-    echo "1. update/download core 🚀"
-    echo "2. update/download mmdb 🗄️"
-    echo "3: update subscription 🔄"
-    echo "4: setting uwp ⚙️"
-    echo "5: run 🐱"
-    echo "0: quit 🔚"
+    Write-Output "Choose operation:"
+    Write-Output "1. update/download core 🚀"
+    Write-Output "2. update/download mmdb 🗄️"
+    Write-Output "3: update subscription 🔄"
+    Write-Output "4: setting uwp ⚙️"
+    Write-Output "5: run 🐱"
+    Write-Output "0: quit 🔚"
 
     $key = [System.Console]::ReadKey($true) # Read-Host "input your instruct"
 
@@ -26,7 +26,7 @@ function Show-Menu {
 
         '1' {
             Clear-Host
-            echo "Start update/download core 🚀"
+            Write-Output "Start update/download core 🚀"
 
             Update-Core
 
@@ -36,7 +36,7 @@ function Show-Menu {
 
         '2' {
             Clear-Host
-            echo "Start update/download mmdb 🗄️"
+            Write-Output "Start update/download mmdb 🗄️"
 
             Update-MMDB
 
@@ -45,7 +45,7 @@ function Show-Menu {
         }
         '3' {
             Clear-Host
-            echo "Start update subscription 🔄"
+            Write-Output "Start update subscription 🔄"
 
 
             Update-Subscription
@@ -56,7 +56,7 @@ function Show-Menu {
 
         '4' {
             Clear-Host
-            echo "Start setting uwp ⚙️"
+            Write-Output "Start setting uwp ⚙️"
 
             Open-UWP-Settings
 
@@ -66,7 +66,7 @@ function Show-Menu {
 
         '5' {
             Clear-Host
-            echo "Start running 🐱"
+            Write-Output "Start running 🐱"
 
             Running-Cat
 
@@ -83,7 +83,7 @@ function Show-Menu {
 
 
         default {
-            echo "`n invaild choose, try again"
+            Write-Output "`n invaild choose, try again"
             Pause
             Show-Menu
         }
@@ -91,7 +91,7 @@ function Show-Menu {
 }
 
 function Pause {
-    echo "Type anything plz..."
+    Write-Output "Type anything plz..."
     [System.Console]::ReadKey($true)
 }
 
@@ -100,10 +100,10 @@ function Init() {
     # We wish environment variables have set
     $env_file = $env:cwd_root + ".env.ps1"
     if (Test-Path -Path $env_file) {
-        echo "load envenvironment variables."
+        Write-Output "load envenvironment variables."
     }
     else {
-        echo "envenvironment file does not exist. refer readme first!"
+        Write-Output "envenvironment file does not exist. refer readme first!"
     }
 
     . $env_file
@@ -131,7 +131,8 @@ function Update-Subscription() {
 
 
 function Update-Core() {
-    $address = "https://github.com/MetaCubeX/mihomo/releases/download/v1.18.8/mihomo-windows-amd64-v1.18.8.zip" 
+    $address = Get-Github-Lastest-Release-Url "MetaCubeX/mihomo"
+
     $origin_path = $env:cwd_data + "core.zip"
 
     fetch $address $origin_path
@@ -152,14 +153,13 @@ function Update-Core() {
 
 
 function Update-MMDB() {
-    $address = "https://github.com/Loyalsoldier/geoip/releases/download/202409190112/Country.mmdb"
-
+    $address = "https://github.com/Loyalsoldier/geoip/releases/latest/download/Country.mmdb"
     fetch $address $env:mmdb
 }
 
 function fetch($url, $target) {
     if (Test-Path -Path $target) {
-        echo "Target file have existed. Skip updating temporarily."
+        Write-Output "Target file have existed. Skip updating temporarily."
     }
     else {
         curl -L $url -o $target
@@ -172,16 +172,16 @@ function Open-UWP-Settings() {
     $installedSoftware = winget list | Select-String -Pattern $softwareName
 
     if ($installedSoftware) {
-        echo "$softwareName has installed, skipping installation"
+        Write-Output "$softwareName has installed, skipping installation"
         $softwarePath = "C:\Program Files (x86)\EnableLoopback\EnableLoopback.exe"
         if (Test-Path $softwarePath) {
             Start-Process $softwarePath
         } else {
-            echo "cannot found executable file, please confirm correct path"
+            Write-Output "cannot found executable file, please confirm correct path"
         }
     }
     else {
-        echo "start installation"
+        Write-Output "start installation"
         
         $uwp_address = "https://telerik-fiddler.s3.amazonaws.com/fiddler/addons/enableloopbackutility.exe"
 
@@ -190,7 +190,7 @@ function Open-UWP-Settings() {
         if (Test-Path $env:uwp) {
             Start-Process $env:uwp
         } else {
-            echo "cannot found executable file, please confirm correct path"
+            Write-Output "cannot found executable file, please confirm correct path"
         }
     }
 
@@ -199,20 +199,44 @@ function Open-UWP-Settings() {
 
 function Running-Cat () {
     if (-not (Test-Path $env:core)) {
-        echo "cannot found core, please confirm have running 1"
+        Write-Output "cannot found core, please confirm have running 1"
         return
     }
     if (-not (Test-Path $env:mmdb)) {
-        echo "cannot found mmdb, please confirm have running 2"
+        Write-Output "cannot found mmdb, please confirm have running 2"
         return
     }
     if (-not (Test-Path $env:config)) {
-        echo "cannot found config, please confirm have running 3"
+        Write-Output "cannot found config, please confirm have running 3"
         return
     }
 
     Start-Process -FilePath $env:core -ArgumentList "-d", $env:cwd_data
 }
+
+function Get-Github-Lastest-Release-Url($repo) {
+    # repo params should be like, MetaCubeX/mihomo
+
+    # Judge current windows cpu architectures
+    $filter = if ($env:PROCESSOR_ARCHITECTURE -eq "AMD64") { "windows-amd64" } else { "windows-arm64" }
+
+    $response = curl -s "https://api.github.com/repos/$repo/releases/latest" | ConvertFrom-Json
+
+    # when we meet the github limit, we should return address to keep it run well
+    if ($response.message -like "*API rate limit exceeded*") {
+        Write-Host "API rate limit exceeded. Please authenticate or try again later."
+        Write-Host "For more information, visit: $($response.documentation_url)"
+        Write-Host "We will use the address build-in."
+        return "https://github.com/MetaCubeX/mihomo/releases/download/v1.18.9/mihomo-windows-amd64-v1.18.9.zip"
+    }
+
+    # get all "browser_download_url" include filter 
+    $windowsLinks = $response.assets | Where-Object { $_.name -match $filter } | Select-Object -ExpandProperty browser_download_url
+
+    # we always get the lastest one.
+    return $windowsLinks[-1]
+}
+
 
 Init
 # TODO: Wait-User
